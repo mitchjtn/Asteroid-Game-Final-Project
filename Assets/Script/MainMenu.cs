c4bd705c5eab7aae8347559fd3188b74700b2cc5
anthonyw29 <63148189+anthonyw29@users.noreply.github.com>
2020-04-12 13:06:15 +0700

    so far..
    blm dilanjutin dr kemarin

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //karna di Unity
+                                                                              // File > Build Settings, Scene Game ada
+                                                                              // di index 1
+    }
+
+    public void SettingGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); // Scene Settings di index 2
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
